58c4d742b196d5cb8867c66ebdb71f163aac32ff
Warp <schiz0yd@yahoo.com>
2015-04-13 11:23:26 -0400

    Create Nutrient_Absorption.lua

---
+++ scripts/globals/mobskills/Nutrient_Absorption.lua
@@ -0,0 +1,26 @@
+---------------------------------------------------
+-- Nutrient Absorption
+-- Steals HP from a single target.
+---------------------------------------------------
+
+require("/scripts/globals/settings");
+require("/scripts/globals/status");
+require("/scripts/globals/monstertpmoves");
+
+---------------------------------------------------
+
+function OnMobSkillCheck(target,mob,skill)
+	return 0;
+end;
+
+function OnMobWeaponSkill(target, mob, skill)
+	local dmgmod = 1;
+	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*2,ELE_DARK,dmgmod,TP_MAB_BONUS,1);
+	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_DARK,MOBPARAM_1_SHADOW);
+
+    target:delHP(dmg);
+    mob:addHP(dmg);
+    skill:setMsg(MSG_DRAIN_HP);
+
+	return dmg;
+end;
