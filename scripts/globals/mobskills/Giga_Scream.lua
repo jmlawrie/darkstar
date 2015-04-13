65dd81171dc86aa058ae5c6925f2ea07f6f4995f
Warp <schiz0yd@yahoo.com>
2015-04-13 11:22:27 -0400

    Create Giga_Scream.lua

---
+++ scripts/globals/mobskills/Giga_Scream.lua
@@ -0,0 +1,28 @@
+---------------------------------------------
+--  Giga_Scream
+--
+--  Description: Delivers a three attack. 
+--  Type: Physical (Blunt)
+--
+--
+---------------------------------------------
+require("/scripts/globals/settings");
+require("/scripts/globals/status");
+require("/scripts/globals/monstertpmoves");
+
+---------------------------------------------
+function OnMobSkillCheck(target,mob,skill)
+	return 0;
+end;
+
+function OnMobWeaponSkill(target, mob, skill)
+
+	local numhits = 3;
+	local accmod = 1;
+	local dmgmod = 1;
+	local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_ACC_VARIES,1,2,3);
+	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_BLUNT,info.hitslanded);
+	target:delHP(dmg);
+
+	return dmg;
+end;
